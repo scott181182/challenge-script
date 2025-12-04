a05c656f1e13e72e131f3f65636919d8dfde3bce
Scott Fasone <scott181182@yahoo.com>
2025-12-03 22:48:20 -0500

    test: added tests for arguments and environment variables

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,9 @@
-use challenge_script::{challenge::{ChallengeCaseError, ChallengeExecutionError, CommandParseError, StringReferenceError}, run_challenge, ChallengeFileError, ProgramError};
-
-
+use challenge_script::{
+    challenge::{
+        ChallengeCaseError, ChallengeExecutionError, CommandParseError, StringReferenceError,
+    },
+    run_challenge, ChallengeFileError, ProgramError,
+};
 
 #[test]
 fn test_success_1() {
@@ -9,26 +12,100 @@ fn test_success_1() {
 
 #[test]
 fn test_success_2() {
-    run_challenge("./tests/data/test2.yml", vec!["group1".to_owned(), "test".to_owned()]).unwrap();
-    run_challenge("./tests/data/test2.yml", vec!["group2".to_owned(), "test".to_owned()]).unwrap();
+    run_challenge(
+        "./tests/data/test2.yml",
+        vec!["group1".to_owned(), "test".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/test2.yml",
+        vec!["group2".to_owned(), "test".to_owned()],
+    )
+    .unwrap();
 }
 
 #[test]
 fn test_success_3() {
-    run_challenge("./tests/data/test3", vec!["group1".to_owned(), "test".to_owned()]).unwrap();
+    run_challenge(
+        "./tests/data/test3",
+        vec!["group1".to_owned(), "test".to_owned()],
+    )
+    .unwrap();
+}
+#[test]
+fn test_success_env() {
+    run_challenge(
+        "./tests/data/env_test.yml",
+        vec!["group1".to_owned(), "toplevel".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/env_test.yml",
+        vec!["group1".to_owned(), "override".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/env_test.yml",
+        vec!["group2".to_owned(), "toplevel".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/env_test.yml",
+        vec!["group2".to_owned(), "override".to_owned()],
+    )
+    .unwrap();
+}
+
+#[test]
+fn test_success_arg() {
+    run_challenge(
+        "./tests/data/arg_test.yml",
+        vec!["group1".to_owned(), "toplevel".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/arg_test.yml",
+        vec!["group1".to_owned(), "override".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/arg_test.yml",
+        vec!["group2".to_owned(), "toplevel".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/arg_test.yml",
+        vec!["group2".to_owned(), "override".to_owned()],
+    )
+    .unwrap();
 }
 
 #[test]
 fn test_success_arguments() {
-    run_challenge("./tests/data/test2.yml", vec!["args".to_owned(), "test2".to_owned()]).unwrap();
-    run_challenge("./tests/data/test2.yml", vec!["args".to_owned(), "test4".to_owned()]).unwrap();
+    run_challenge(
+        "./tests/data/test2.yml",
+        vec!["args".to_owned(), "test2".to_owned()],
+    )
+    .unwrap();
+    run_challenge(
+        "./tests/data/test2.yml",
+        vec!["args".to_owned(), "test4".to_owned()],
+    )
+    .unwrap();
 }
 
 #[test]
 fn test_error_expected() {
-    let res = run_challenge("./tests/data/test3", vec!["group1".to_owned(), "error".to_owned()]);
+    let res = run_challenge(
+        "./tests/data/test3",
+        vec!["group1".to_owned(), "error".to_owned()],
+    );
 
-    if let Err(ProgramError::ExecutionError(ChallengeExecutionError::UnexpectedOutput{ expected, actual})) = res {
+    if let Err(ProgramError::ExecutionError(ChallengeExecutionError::UnexpectedOutput {
+        expected,
+        actual,
+    })) = res
+    {
         assert_eq!(expected, "test_1");
         assert_eq!(actual, "test_2");
     } else {
@@ -38,9 +115,16 @@ fn test_error_expected() {
 
 #[test]
 fn test_error_case_not_found_root() {
-    let res = run_challenge("./tests/data/test3", vec!["group2".to_owned(), "error".to_owned()]);
+    let res = run_challenge(
+        "./tests/data/test3",
+        vec!["group2".to_owned(), "error".to_owned()],
+    );
 
-    if let Err(ProgramError::InputCaseError(ChallengeCaseError::CaseNotFound { case, config_name })) = res {
+    if let Err(ProgramError::InputCaseError(ChallengeCaseError::CaseNotFound {
+        case,
+        config_name,
+    })) = res
+    {
         assert_eq!(config_name, "Test 3");
         assert_eq!(case, "group2");
     } else {
@@ -50,9 +134,16 @@ fn test_error_case_not_found_root() {
 
 #[test]
 fn test_error_case_not_found_nested() {
-    let res = run_challenge("./tests/data/test3", vec!["group1".to_owned(), "nonexistent".to_owned()]);
+    let res = run_challenge(
+        "./tests/data/test3",
+        vec!["group1".to_owned(), "nonexistent".to_owned()],
+    );
 
-    if let Err(ProgramError::InputCaseError(ChallengeCaseError::CaseNotFound { case, config_name })) = res {
+    if let Err(ProgramError::InputCaseError(ChallengeCaseError::CaseNotFound {
+        case,
+        config_name,
+    })) = res
+    {
         assert_eq!(config_name, "group1");
         assert_eq!(case, "nonexistent");
     } else {
@@ -64,21 +155,40 @@ fn test_error_case_not_found_nested() {
 fn test_error_case_not_enough_cases() {
     let res = run_challenge("./tests/data/test3", vec!["group1".to_owned()]);
 
-    assert!(matches!(res, Err(ProgramError::InputCaseError(ChallengeCaseError::NotEnoughCases))));
+    assert!(matches!(
+        res,
+        Err(ProgramError::InputCaseError(
+            ChallengeCaseError::NotEnoughCases
+        ))
+    ));
 }
 
 #[test]
 fn test_error_empty_command() {
-    let res = run_challenge("./tests/data/bad.yml", vec!["empty_command".to_owned(), "doesn't matter".to_owned()]);
+    let res = run_challenge(
+        "./tests/data/bad.yml",
+        vec!["empty_command".to_owned(), "doesn't matter".to_owned()],
+    );
 
-    assert!(matches!(res, Err(ProgramError::ExecutionError(ChallengeExecutionError::BadCommand(CommandParseError::EmptyCommand)))));
+    assert!(matches!(
+        res,
+        Err(ProgramError::ExecutionError(
+            ChallengeExecutionError::BadCommand(CommandParseError::EmptyCommand)
+        ))
+    ));
 }
 
 #[test]
 fn test_error_malformed_command() {
-    let res = run_challenge("./tests/data/bad.yml", vec!["malformed_command".to_owned(), "whatever".to_owned()]);
+    let res = run_challenge(
+        "./tests/data/bad.yml",
+        vec!["malformed_command".to_owned(), "whatever".to_owned()],
+    );
 
-    if let Err(ProgramError::ExecutionError(ChallengeExecutionError::BadCommand(CommandParseError::MalformedString(cmd)))) = res {
+    if let Err(ProgramError::ExecutionError(ChallengeExecutionError::BadCommand(
+        CommandParseError::MalformedString(cmd),
+    ))) = res
+    {
         assert_eq!(cmd, "echo \"not closed");
     } else {
         panic!("Unexpected result: {:?}", res);
@@ -87,20 +197,38 @@ fn test_error_malformed_command() {
 
 #[test]
 fn test_error_input_file_not_found() {
-    let res = run_challenge("./tests/data/bad.yml", vec!["bad_input".to_owned(), "nonexistent".to_owned()]);
+    let res = run_challenge(
+        "./tests/data/bad.yml",
+        vec!["bad_input".to_owned(), "nonexistent".to_owned()],
+    );
 
-    assert!(matches!(res, Err(ProgramError::ExecutionError(ChallengeExecutionError::BadStringReference(StringReferenceError::FileRead(_))))));
+    assert!(matches!(
+        res,
+        Err(ProgramError::ExecutionError(
+            ChallengeExecutionError::BadStringReference(StringReferenceError::FileRead(_))
+        ))
+    ));
 }
 
 #[test]
 fn test_error_challenge_file_not_found() {
     let res = run_challenge("./tests/data/empty/challenge.yml", vec![]);
 
-    assert!(matches!(res, Err(ProgramError::InputFileError(ChallengeFileError::FileDoesNotExist(_)))));
+    assert!(matches!(
+        res,
+        Err(ProgramError::InputFileError(
+            ChallengeFileError::FileDoesNotExist(_)
+        ))
+    ));
 }
 #[test]
 fn test_error_challenge_file_not_found_in_directory() {
     let res = run_challenge("./tests/data/empty", vec![]);
 
-    assert!(matches!(res, Err(ProgramError::InputFileError(ChallengeFileError::FileNotFoundInDirectory(_)))));
+    assert!(matches!(
+        res,
+        Err(ProgramError::InputFileError(
+            ChallengeFileError::FileNotFoundInDirectory(_)
+        ))
+    ));
 }
